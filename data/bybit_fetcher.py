5348a0fcb7171c50c45f74d09e6ef229a2e9fd1b
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-06-19 22:08:10 +0100

    Update bybit_fetcher.py

--- data/bybit_fetcher.py
+++ data/bybit_fetcher.py
@@ -1,1 +1,57 @@
+import requests
+import pandas as pd
 
+from config.settings import (
+    BYBIT_BASE_URL,
+    TIMEFRAME,
+    CANDLE_LIMIT
+)
+
+
+def get_klines(symbol):
+
+    url = f"{BYBIT_BASE_URL}/v5/market/kline"
+
+    params = {
+        "category": "linear",
+        "symbol": symbol,
+        "interval": TIMEFRAME,
+        "limit": CANDLE_LIMIT
+    }
+
+    response = requests.get(
+        url,
+        params=params,
+        timeout=20
+    )
+
+    data = response.json()
+
+    if data["retCode"] != 0:
+        raise Exception(data)
+
+    candles = data["result"]["list"]
+
+    rows = []
+
+    for c in candles:
+
+        rows.append({
+            "timestamp": int(c[0]),
+            "open": float(c[1]),
+            "high": float(c[2]),
+            "low": float(c[3]),
+            "close": float(c[4]),
+            "volume": float(c[5])
+        })
+
+    df = pd.DataFrame(rows)
+
+    df = df.sort_values("timestamp")
+
+    df.reset_index(
+        drop=True,
+        inplace=True
+    )
+
+    return df
