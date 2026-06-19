60515986f60637dc437f312aa59b1b8c4607e873
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-06-19 22:10:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from data.bybit_fetcher import get_klines
 
+def main():
+
+    symbol = "BTCUSDT"
+
+    df = get_klines(symbol)
+
+    print(df.tail())
+
+    print()
+    print(f"{symbol} loaded")
+    print(f"Candles: {len(df)}")
+
+if __name__ == "__main__":
+    main()
