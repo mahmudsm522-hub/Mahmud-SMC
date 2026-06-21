be88e3d5dc694dc1efa174cd1909f122833d813c
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-06-21 15:38:48 +0100

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,32 @@
+import logging
+import os
 
+LOG_DIR = "logs"
+
+os.makedirs(
+    LOG_DIR,
+    exist_ok=True
+)
+
+LOG_FILE = (
+    f"{LOG_DIR}/bot.log"
+)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=(
+        "%(asctime)s | "
+        "%(levelname)s | "
+        "%(message)s"
+    ),
+    handlers=[
+        logging.FileHandler(
+            LOG_FILE
+        ),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(
+    "SMC_BOT"
+)
