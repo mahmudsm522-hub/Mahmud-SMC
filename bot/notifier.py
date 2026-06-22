6b29223a7d36131ea7e57d56e45291d804dded8c
mahmudsm522-hub <mahmudsm522@gmail.com>
2026-06-22 20:23:14 +0100

    Update notifier.py

--- bot/notifier.py
+++ bot/notifier.py
@@ -8,20 +8,42 @@ from config.settings import (
 
 def send_message(text):
 
-    url = (
-        f"https://api.telegram.org/"
-        f"bot{BOT_TOKEN}/sendMessage"
-    )
+    if not BOT_TOKEN:
+        print(
+            "[ERROR] BOT_TOKEN missing"
+        )
+        return None
 
-    payload = {
-        "chat_id": CHAT_ID,
-        "text": text
-    }
+    if not CHAT_ID:
+        print(
+            "[ERROR] CHAT_ID missing"
+        )
+        return None
 
-    response = requests.post(
-        url,
-        json=payload,
-        timeout=20
-    )
+    try:
 
-    return response.json()
+        url = (
+            f"https://api.telegram.org/"
+            f"bot{BOT_TOKEN}/sendMessage"
+        )
+
+        payload = {
+            "chat_id": CHAT_ID,
+            "text": text
+        }
+
+        response = requests.post(
+            url,
+            json=payload,
+            timeout=20
+        )
+
+        return response.json()
+
+    except Exception as e:
+
+        print(
+            f"[TELEGRAM ERROR] {e}"
+        )
+
+        return None
